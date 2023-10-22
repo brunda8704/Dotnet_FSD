bd95363007f67520f425fffe4b7372a41024968e
Brunda_Mahesh <64202436+brunda8704@users.noreply.github.com>
2023-10-22 19:06:20 +0530

    Add files via upload

---
+++ CSharp_Fundamentals/SortingComplexTypes/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace SortingComplexTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SortingComplexTypes!\n");
+
+            List<Student> students = new List<Student>()
+            { new Student{ID=1,Name="Dhanush",Marks=80},
+            new Student{ID=2,Name="Anu",Marks=60}
+            };
+            Student student = new Student();
+
+            Console.WriteLine("Before Sorting");
+            foreach (Student student1 in students)
+            {
+                Console.WriteLine($"Id:{student1.ID},Name:{student1.Name},Marks:{student1.Marks}");
+            }
+
+            //SortByMarks sortByMarks = new SortByMarks();
+            //students.Sort(sortByMarks);
+
+            students.Sort();
+            Console.WriteLine("\nAfter Sorting");
+            foreach (Student student1 in students)
+            {
+                Console.WriteLine($"Id:{student1.ID},Name:{student1.Name},Marks:{student1.Marks} ");
+            }
+
+        }
+    }
+}
---
+++ CSharp_Fundamentals/SortingComplexTypes/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingComplexTypes
+{
+    internal class Student:IComparable<Student>
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Marks { get; set; }
+
+        public int CompareTo(Student other)
+        {
+            //if(this.Marks>other.Marks)
+            //    return 1;
+            //else if(this.Marks<other.Marks)
+            //    return -1;
+            //else
+            //    return 0;
+
+            return this.Marks.CompareTo(other.Marks);
+        }
+
+        //public class SortByMarks : IComparer<Student>
+        //{
+        //    public int Compare(Student x, Student y)
+        //    {
+        //        return x.Marks.CompareTo(y.Marks);
+        //    }
+        //}
+    }
+
+}
