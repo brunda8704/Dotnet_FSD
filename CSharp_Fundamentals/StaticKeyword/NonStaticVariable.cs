466434a17d5443a2083e7338b61231a2e4b483f8
Brunda_Mahesh <64202436+brunda8704@users.noreply.github.com>
2023-10-22 19:03:54 +0530

    Add files via upload

---
+++ CSharp_Fundamentals/StaticKeyword/NonStaticVariable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticKeyword
+{
+    internal class NonStaticVariable
+    {
+        public string place;
+    }
+}
---
+++ CSharp_Fundamentals/StaticKeyword/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace StaticKeyword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Static Variable
+            //-> can be accessed using the class name
+            Console.WriteLine("Department: " + StaticVariable.department);
+
+            //Non-static Variable
+            //-> for accessing non-static variable we need to create objects of the class 
+            NonStaticVariable nonstatic = new NonStaticVariable();
+            nonstatic.place = "Mysore";
+            Console.WriteLine("Place: " + nonstatic.place);
+
+            // Static Methods
+            //-> Just like static variables, we can call the static methods using the class name.
+
+            // Non-static Methods
+            //-> Just like non-static variables, we need to create objects of the class
+
+            // Static Class
+            //-> when we declare a class as static, we cannot create objects of the class
+            //-> we can only have static members inside the static class.
+            //-> we cannot inherit a static class 
+
+            /* example for static class:
+             * 
+             * static class Test {
+               static int a = 5;
+               static void display() 
+             * {
+
+                    Console.WriteLine("Static method");
+               }
+             * 
+             */
+
+            //private constructor-> we cannot create objects of the class
+
+            // Main method is static-> so we can call it without creating the object.
+
+        }
+    }
+}
