f9f82afe5cddde5caaa136bee65bb80313d874ba
Brunda_Mahesh <64202436+brunda8704@users.noreply.github.com>
2023-10-22 19:05:49 +0530

    Add files via upload

---
+++ CSharp_Fundamentals/ThreadDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ThreadDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main thread started!");
+            Thread threadOne = new Thread(Display)
+            {
+                Name = "Thead One"
+            };
+            threadOne.Start();
+            
+            Thread threadTwo = new Thread(Show);
+            threadTwo.Start();
+            Console.WriteLine("Main thread ended!");
+        }
+
+        static void Display()
+        {
+            Console.WriteLine("Display method started using " + Thread.CurrentThread.Name);
+            Console.WriteLine("Display method");
+        }
+
+        static void Show()
+        {
+            Thread.Sleep(1000); 
+            Console.WriteLine("Show method");
+        }
+    }
+}
