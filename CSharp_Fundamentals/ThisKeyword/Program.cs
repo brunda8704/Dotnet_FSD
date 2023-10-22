65537f7dc28729d0ccd3c52e623f7d5dc2ac1574
Brunda_Mahesh <64202436+brunda8704@users.noreply.github.com>
2023-10-22 19:06:52 +0530

    Add files via upload

---
+++ CSharp_Fundamentals/ThisKeyword/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ThisKeyword
+{
+    internal class Program
+    {
+
+        //constructor overloading
+        //Calling one constructor from another constructor-> constructor chaining
+
+        Program()
+        {
+            Console.WriteLine("default constructor");
+        }
+        Program(int number1, int number2)
+        {
+            Console.WriteLine("Constructor with two parameter");
+        }
+
+
+        Program(int number) : this(33, 22)    //1st it invokes the constructor with 2 parameters
+        {
+            Console.WriteLine("Constructor with one parameter");
+        }
+
+
+        static void Main(string[] args)
+        {
+            Program program = new Program(10);
+            Console.ReadLine();
+        }
+
+    }
+}
+ 
